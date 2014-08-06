04eefb4e7dfcd684a5e307b14dbc00c5a81f5379
christinaleuci <christinaleuci@gmail.com>
2014-08-06 14:39:20 -0400

    working on making week, month, semester

--- app/controllers/data_scrape_controller.rb
+++ app/controllers/data_scrape_controller.rb
@@ -6,23 +6,20 @@ class DataScrapeController < ApplicationController
       {'sDate' => 'today',
        'sTime' => '13:30', 
        'sUsername' => '@jessrudder',
-       'nStatus' => 1,
-       'nPullRequests' => 170,
-       'fLight' => true
+       'sTimeFrame' => 'week',
+       'nPullRequests' => 170
       },
       {'sDate' => 'yesterday', 
        'sTime' => '16:00',
        'sUsername' => '@denineguy',
-       'nStatus' => 2,
-       'nPullRequests' => 19,
-       'fLight' =>false
+       'sTimeFrame' => 'week',
+       'nPullRequests' => 19
       },
       {'sDate' => 'July 8th, 2013',
        'sTime' => '16:30',
        'sUsername' => '@christinaleuci',
-       'nStatus' => 2,
-       'nPullRequests' => 1,
-       'fLight' => false
+       'sTimeFrame' => 'week',
+       'nPullRequests' => 1
       }
    ].to_json
 
