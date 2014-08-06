0694f89490e93df89b4d1cc1011504361d43152b
christinaleuci <christinaleuci@gmail.com>
2014-08-06 16:26:33 -0400

    work

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
 
   # get '/data' => 'data#index'
   resources :data, only: [:index, :show]
+  get '/index.html' => 'data_scrape#index' 
+  get '/solari_data' => 'data_scrape#solari_data'
 end
