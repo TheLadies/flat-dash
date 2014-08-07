3d30c8d194bfb171a8a5b1d9a6863c64cbec5fb1
Denine Guy <denineguy@gmail.com>
2014-08-07 15:35:40 -0400

    updated repository file

--- app/models/repository.rb
+++ app/models/repository.rb
@@ -40,6 +40,23 @@ class Repository < ActiveRecord::Base
     end
   end
 
+  def self.top_pull_requests
+    pull_counts_array = []
+    student_pulls = Repository.group(:user_login).order("count_all DESC").calculate(:count, :all)
+    users = student_pulls.keys
+    pulls = student_pulls.values
+    users.each_with_index do |user, i|
+      pull_counts_array << ({:sDate =>"today", :sTime => "13:30", :sUsername => "@"+ user, :sTimeFrame => "week", :nPullRequests => pulls[i]}) 
+    end
+  pull_counts_array
+  end
+
+  def self.todays_pull_requests
+    pull_counts_array = []
+    Repository.where("pull_updated_at > ?", 7.days.ago).group(:user_login).order("count_all DESC").calculate(:count, :all)
+    # student_pulls = Repository.where().group(:user_login).order("count_all DESC").calculate(:count, :all)
+  end
+end  
   # def self.top_pull_requests
   #   students = []
   #   self.all.each do |repo|
@@ -78,17 +95,8 @@ class Repository < ActiveRecord::Base
   # end
 
   # Today 
-  def self.top_pull_requests
-    pull_counts_array = []
-    student_pulls = Repository.group(:user_login).order("count_all DESC").calculate(:count, :all)
-    users = student_pulls.keys
-    pulls = student_pulls.values
-    users.each_with_index do |user, i|
-      pull_counts_array << ({:sDate =>"today", :sTime => "13:30", :sUsername => "@"+ user, :sTimeFrame => "week", :nPullRequests => pulls[i]}) 
-    end
-  pull_counts_array
-  end
-end
+   
+
 
 # Last days 7 days  
 
