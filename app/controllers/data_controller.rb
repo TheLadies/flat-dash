a2d3b6d5fdee12e95aa166d77b4587e7cc26bc3e
Jess Rudder <thejessicarudder@gmail.com>
2014-08-07 11:56:26 -0400

    Adds show to data controller

--- app/controllers/data_controller.rb
+++ app/controllers/data_controller.rb
@@ -7,7 +7,14 @@ class DataController < ApplicationController
     end
 
     def show
-      render "#{params[:id]}.html"
+      @pull_count = Repository.pull_request_by_user
+
+      respond_to do |f|
+        f.html {render "#{params[:id]}.html"}
+        f.json {render json: @pull_count }
+      end
+      
+      
     end
 
 end
