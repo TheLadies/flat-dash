e622088c5de05ede82f98df74e6e6c61364bdf0b
Denine Guy <denineguy@gmail.com>
2014-08-12 13:38:39 -0400

    added pull updated at to group

--- app/models/repository.rb
+++ app/models/repository.rb
@@ -32,7 +32,7 @@ class Repository < ActiveRecord::Base
     pull_counts_array = []
     # binding.pry
     # pull_dates = Repository.group(:user_login).order("pull_updated_at DESC").maximum(:pull_updated_at)
-    pull_dates = Repository.order("pull_updated_at DESC").group(:user_login).maximum(:pull_updated_at)
+    pull_dates = Repository.group(:user_login, :pull_updated_at).order("pull_updated_at DESC").maximum(:pull_updated_at)
     student_pulls = Repository.group(:user_login).order("count_all DESC").calculate(:count, :all)
     users = student_pulls.keys
     count = student_pulls.values
@@ -46,7 +46,7 @@ class Repository < ActiveRecord::Base
 
   def self.todays_pull_requests
     pull_counts_array = []
-    pull_dates = Repository.group(:user_login).order("pull_updated_at DESC").maximum(:pull_updated_at)
+    pull_dates = Repository.group(:user_login, :pull_updated_at).order("pull_updated_at DESC").maximum(:pull_updated_at)
     student_pulls = Repository.where("pull_updated_at > ?", 1.days.ago).group(:user_login).order("count_all DESC").calculate(:count, :all)
     users = student_pulls.keys
     count = student_pulls.values
